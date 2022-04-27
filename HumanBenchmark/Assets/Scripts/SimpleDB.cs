1c8d85bf5c9cb20423d4adac0ce08ce6dd2725f1
Mantas Brazdžius <mantasbr4@gmail.com>
2022-04-27 19:45:31 +0300

    Setting up Database methods

---
+++ HumanBenchmark/Assets/Scripts/SimpleDB.cs
@@ -0,0 +1,31 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mono.Data.Sqlite;
+
+public class SimpleDB : MonoBehaviour
+{
+    private string databaseName = "URI=file:HumanBenchmark.db";
+
+    void Start()
+    {
+        //CreateDB();
+        //CreateUser("Mantas", "Slaptazodis");
+        //GetUsers();
+    }
+
+    public void CreateDB() 
+    {
+
+    }
+
+    public void CreateUser(string username, string password)
+    {
+        
+    }
+
+    public void GetUsers()
+    {
+       
+    }
+}
