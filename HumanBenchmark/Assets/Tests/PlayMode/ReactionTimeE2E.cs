da7b3c44f07d8b5e371dc78c5bfbc19aa100ac0c
mantasql <mantas.miliukas5@gmail.com>
2022-05-01 01:22:56 +0300

    Refactor ReactionTimeE2E tests

--- HumanBenchmark/Assets/Tests/PlayMode/ReactionTimeE2E.cs
+++ HumanBenchmark/Assets/Tests/PlayMode/ReactionTimeE2E.cs
@@ -9,46 +9,108 @@ using TMPro;
 
 public class ReactionTimeE2E
 {
-    private void LoadScene()
-    {
-        SceneManager.LoadScene("MainMenu");
-    }
+    private ReactionTimeGame reactionTimeGame;
 
     [UnityTest]
-    public IEnumerator ReactionTimeGame()
+    public IEnumerator When_ClickReactionTimeGameButton_Expect_LoadReactionTimeGame()
     {
-
-        LoadScene();
+        SceneManager.LoadScene("MainMenu");
         yield return null;
 
         GameObject.Find("ReactionTimeUIButton").GetComponent<Button>().onClick.Invoke();
         yield return null;
-
         Assert.AreEqual("ReactionTimeGame",SceneManager.GetActiveScene().name);
+    }
 
-        ReactionTimeGame reactionTimeGame = GameObject.Find("ClickArea").GetComponent<ReactionTimeGame>();
+    [UnityTest]
+    public IEnumerator When_ClickGameStartButton_Expect_StartTheGame()
+    {
+        SceneManager.LoadScene("ReactionTimeGame");
+        yield return null;
+
+        reactionTimeGame = GameObject.Find("ClickArea").GetComponent<ReactionTimeGame>();
 
         GameObject.Find("ClickArea").GetComponent<Button>().onClick.Invoke();
         yield return null;
         Assert.AreEqual(true,reactionTimeGame.IsGameRunning());
+    }
+
+    [UnityTest]
+    public IEnumerator When_PressGameWindowWhenItsRed_Expect_GameToEnd()
+    {
+        SceneManager.LoadScene("ReactionTimeGame");
+        yield return null;
+        
+        reactionTimeGame = GameObject.Find("ClickArea").GetComponent<ReactionTimeGame>();
+
+        GameObject.Find("ClickArea").GetComponent<Button>().onClick.Invoke();
+        yield return null;
 
         GameObject.Find("ClickArea").GetComponent<Button>().onClick.Invoke();
         yield return null;
         Assert.AreEqual(false,reactionTimeGame.IsGameRunning());
+    }    
+
+    [UnityTest]
+    public IEnumerator When_PressRestartButton_Expect_StartGame()
+    {
+        SceneManager.LoadScene("ReactionTimeGame");
+        yield return null;
+
+        reactionTimeGame = GameObject.Find("ClickArea").GetComponent<ReactionTimeGame>();
+
+        GameObject.Find("ClickArea").GetComponent<Button>().onClick.Invoke();
+        yield return null;
+
+        GameObject.Find("ClickArea").GetComponent<Button>().onClick.Invoke();
+        yield return null;
 
         GameObject.Find("ClickArea").GetComponent<Button>().onClick.Invoke();
         yield return null;
         Assert.AreEqual(true,reactionTimeGame.IsGameRunning());
+    }
 
+    [UnityTest]
+    public IEnumerator When_WaitForWindowToTurnGreen_Expect_TimerCanBeStopped()
+    { 
+        SceneManager.LoadScene("ReactionTimeGame");
+        yield return null;
+
+        GameObject.Find("ClickArea").GetComponent<Button>().onClick.Invoke();
+        yield return null;
+
+        reactionTimeGame = GameObject.Find("ClickArea").GetComponent<ReactionTimeGame>();
+        yield return new WaitForSeconds(6);
+        Assert.AreEqual(true,reactionTimeGame.TimerCanBeStopped);
+    }   
+
+    [UnityTest]
+    public IEnumerator When_PressGameWindowWhenItsGreen_Expect_GameToEnd()
+    { 
+        SceneManager.LoadScene("ReactionTimeGame");
+        yield return null;
+
+        GameObject.Find("ClickArea").GetComponent<Button>().onClick.Invoke();
+        yield return null;
+
+        reactionTimeGame = GameObject.Find("ClickArea").GetComponent<ReactionTimeGame>();
         yield return new WaitForSeconds(6);
 
-        Assert.AreEqual(true,reactionTimeGame.TimerCanBeStopped);
         GameObject.Find("ClickArea").GetComponent<Button>().onClick.Invoke();
         yield return null;
+
         Assert.AreEqual(false,reactionTimeGame.IsGameRunning());
+    }
+
+    [UnityTest]
+    public IEnumerator When_BackButtonIsPressed_Expect_LoadMainMenu()
+    { 
+        SceneManager.LoadScene("ReactionTimeGame");
+        yield return null;
 
         GameObject.Find("BackToMenu").GetComponent<Button>().onClick.Invoke();
         yield return null;
         Assert.AreEqual("MainMenu",SceneManager.GetActiveScene().name); 
     }
+
 }
